35911af13ff98167bf01e09f3cd34eda140c88e7
xingyaoc <xchen@eit.org>
2024-09-04 00:26:06 +0000

    working on agg;

--- protein_design_eval/pipeline/standard/base.py
+++ protein_design_eval/pipeline/standard/base.py
@@ -251,27 +251,27 @@ class Pipeline(ABC):
 		# Process
 		for domain in tqdm(domains, desc=f'Aggregating scores for {domains}', disable=not verbose):
 
-			# Find best sample based on scRMSD
-			resample_idxs, scrmsds = [], []
-			for filepath in glob.glob(os.path.join(scores_dir, f'{domain}-resample_*.txt')):
-				resample_idx = int(filepath.split('_')[-1].split('.')[0])
-				resample_results = parse_tm_file(filepath)
-				resample_idxs.append(resample_idx)
-				scrmsds.append(resample_results['rmsd'])
-			best_resample_idx = resample_idxs[np.argmin(scrmsds)]
+			# # Find best sample based on scRMSD
+			# resample_idxs, scrmsds = [], []
+			# for filepath in glob.glob(os.path.join(scores_dir, f'{domain}-resample_*.txt')):
+			# 	resample_idx = int(filepath.split('_')[-1].split('.')[0])
+			# 	resample_results = parse_tm_file(filepath)
+			# 	resample_idxs.append(resample_idx)
+			# 	scrmsds.append(resample_results['rmsd'])
+			# best_resample_idx = resample_idxs[np.argmin(scrmsds)]
 
 			# Parse scores
 			tm_filepath = os.path.join(
 				scores_dir,
-				f'{domain}-resample_{best_resample_idx}.txt'
+				f'{domain}.txt'
 			)
 			output = parse_tm_file(tm_filepath)
 			sctm, scrmsd, seqlen = output['tm'], output['rmsd'], output['seqlen']
 
 			# Parse pLDDT
 			pdb_filepath = os.path.join(
-				structures_dir,
-				f'{domain}-resample_{best_resample_idx}.pdb'
+				structures_dir, domain,
+				f'{domain}.pdb'
 			)
 			output = parse_pdb_file(pdb_filepath)
 			plddt = np.mean(output['pLDDT'])
@@ -279,7 +279,7 @@ class Pipeline(ABC):
 			# Parse pAE
 			pae_filepath = os.path.join(
 				structures_dir,
-				f'{domain}-resample_{best_resample_idx}.pae.txt'
+				f'{domain}.pae.txt'
 			)
 			pae = parse_pae_file(pae_filepath)['pAE'] if os.path.exists(pae_filepath) else None
 
